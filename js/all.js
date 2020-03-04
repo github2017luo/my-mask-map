ccf670029f3a2efd6117fec0faa2be1bcc2cd66a
stephenlaichaowen <48738466+stephenlaichaowen@users.noreply.github.com>
2020-03-04 22:25:24 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -272,8 +272,7 @@ sideBarSwitchBtn.addEventListener('click', sideBarOpenAndClose);
     center: [25.04828, 121.51435],
     zoom: 16 });
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '作者 Albert || 設計師 Wendy ' }).
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').
   addTo(map);
   // 使用 control.locate 套件
   L.control.
